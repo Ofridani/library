4360b39fc75e22542a8f7e38b4bbb80746d362d3
Ofridani <104002869+Ofridani@users.noreply.github.com>
2025-01-25 15:37:39 +0200

    Changed book class display and page to pages

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const myLibrary = [];
 
-function Book(title, author, page, read) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.page = page;
+    this.pages = pages;
     this.read = read;
 
     this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages}, ${this.read ? "already read" : "not read yet"}`
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`
     }
 }
+
+const b1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+console.log(b1.info());
