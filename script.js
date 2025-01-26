05c89754e022f27f7f46ddeddaa9cf839327d086
Ofridani <104002869+Ofridani@users.noreply.github.com>
2025-01-26 02:21:26 +0200

    Created displayLibrary function

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 
-    this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`
-    }
+    // this.info = function(){
+    //     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`
+    // }
 }
 
 function addBookToLibrary(title, author, pages, read){
@@ -18,3 +18,18 @@ function addBookToLibrary(title, author, pages, read){
 const bookTable = document.querySelector("table");
 const totalBooksElement = bookTable.querySelector("#totalBooks").nextSibling;
 const totalPagesElement = bookTable.querySelector("#totalPages").nextSibling;
+
+function displayLibrary(){
+    myLibrary.forEach(book => {
+        const row = document.createElement("tr")
+        for (const propertyName of Object.getOwnPropertyNames(book)) {
+            const col = document.createElement("td");
+            col.textContent = propertyName == "read" ? (book[propertyName] ? "Yes" : "No"): book[propertyName];
+            row.appendChild(col);
+        }
+        bookTable.querySelector("tbody").appendChild(row);
+    });
+}
+
+addBookToLibrary("Eragon", "Christopher Paolini", 532, true)
+displayLibrary()
